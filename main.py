b4cc0d6afa1f4f4c275bd5459f20fc462ad49737
Kalungez <muthomititus+github@gmail.com>
2020-02-12 17:55:50 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template
+import pygal
+import psycopg2
+
+app = Flask(__name__)
+# conn = psycopg2.connect("dbname='postgres' user='postgres' password='dbpass'")
+
+@app.route('/')
+def home():
+
+    conn = psycopg2.connect("dbname = 'postgres' user='postgres' host='localhost' password='123456'")
+
+    cur = conn.cursor()
+
+    cur.execute("""SELECT EXTRACT (MONTHS FROM sales.created_at) AS months,
+    SUM(sales.quantity) as "Total Sales" 
+    FROM sales
+    GROUP BY 
+    months 
+    ORDER BY 
+    months""")
+    records = cur.fetchall()
+
+    xlabels = []
+    sales = []
+
+    for i in records:
+        xlabels.append(i[0])
+        sales.append(i[1])
+
+
+
+
+    line_chart = pygal.Line()
+    line_chart.title = 'Sales total in each month'
+    line_chart.x_labels = map(str, xlabels)
+    line_chart.add('Sales', sales)
+    line_data = line_chart.render_data_uri()
+
+
+
+    data = [
+    ('Internet Explorer', 19.5), 
+    ('Firefox', 36.6),
+    ('Chrome', 36.3), 
+    ('Safari', 4.5), 
+    ('Opera', 2.3)
+    ]
+
+
+    pie_chart = pygal.Pie()
+    pie_chart.title = 'Browser usage in February 2012 (in %)'
+    pie_chart.add(data[0][0], data[0][1])
+    pie_chart.add(data[1][0], data[1][1])
+    pie_chart.add(data[2][0], data[2][1])
+    pie_chart.add(data[3][0], data[3][1])
+    pie_chart.add(data[4][0], data[4][1])
+
+    pie_data = pie_chart.render_data_uri()
+ 
+   
+
+
+    return render_template('index.html', pie_data=pie_data, line_data=line_data)
+
+@app.route('/person/<name>/<int:age>')
+def person(name, age):
+    return f'{name} is {age} years old'
+
+
+@app.route('/numbers/<int:firstNum>/<int:secondNum>')
+def add(firstNum, secondNum):
+    summation = firstNum + secondNum
+    return f'the sum of {firstNum} and {secondNum} is {summation}'
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'This is about page')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+    
